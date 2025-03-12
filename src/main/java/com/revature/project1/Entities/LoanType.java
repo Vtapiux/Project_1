c5eb535a4fc266e937ab2705b0860a193edf9ee7
Juan Carlos Bravo M. <juancarlos99f@hotmail.com>
2025-03-12 14:19:43 -0600

    LoanTyp, LoanStatus, Role 2.0

--- src/main/java/com/revature/project1/Entities/LoanType.java
+++ src/main/java/com/revature/project1/Entities/LoanType.java
@@ -3,6 +3,7 @@ package com.revature.project1.Entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -10,7 +11,7 @@ import jakarta.persistence.Table;
 @Table(name = "Loan_Type")
 public class LoanType {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "loan_type_id", nullable = false)
     private Long id;
 
