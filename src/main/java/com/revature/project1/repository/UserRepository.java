78d3d523f47a7e547bf10c242e34423eef8fd842
Hector <megahector192@gmail.com>
2025-03-22 14:24:58 -0600

    Uploading user controller,service,service implementation and repository with update endpoints and get info per user endpoint

--- src/main/java/com/revature/project1/repository/UserRepository.java
+++ src/main/java/com/revature/project1/repository/UserRepository.java
@@ -4,4 +4,5 @@ import com.revature.project1.Entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,Long> {
+    User findByAccount_accountId(Long idAccount);
 }
