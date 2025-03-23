596e00c54b09124943b5666dd0579f38d48e71cf
vale_tapiux <v.tapia@lasallistas.org.mx>
2025-03-23 16:07:22 -0600

    Finished validations on Address

--- src/main/java/com/revature/project1/controller/AddressController.java
+++ src/main/java/com/revature/project1/controller/AddressController.java
@@ -2,6 +2,8 @@ package com.revature.project1.controller;
 
 import com.revature.project1.Entities.Account;
 import com.revature.project1.Entities.Address;
+import com.revature.project1.Entities.User;
+import com.revature.project1.repository.UserRepository;
 import com.revature.project1.service.AddressService;
 import com.revature.project1.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,11 +18,13 @@ import java.util.*;
 @RequestMapping("/address")
 public class AddressController {
     private final AddressService addressService;
+    private final UserRepository userRepository;
     private final UserService userService;
 
-    public AddressController(AddressService addressService, UserService userService) {
+    public AddressController(AddressService addressService, UserService userService, UserRepository userRepository, UserService userService1) {
         this.addressService = addressService;
-        this.userService = userService;
+        this.userRepository = userRepository;
+        this.userService = userService1;
     }
 
     @GetMapping
@@ -42,29 +46,93 @@ public class AddressController {
     @GetMapping("/{id}")
     public ResponseEntity<?> getAddressById(@PathVariable Long id, HttpServletRequest httpServletRequest){
         if(httpServletRequest.getSession(false) != null){
+            //Validate if a session is in progress
             HttpSession httpSession = httpServletRequest.getSession(false);
             Account account = (Account) httpSession.getAttribute("newAccount");
-
-            if()
+            if(account.getRole().getRoleId() == 2) {
+                //Is a manager
+                if(addressService.findAddressById(id).isEmpty()){
+                    return ResponseEntity.ok("error: No address found!");
+                } else {
+                    return addressService.findAddressById(id)
+                            .map(ResponseEntity::ok)
+                            .orElse(ResponseEntity.notFound().build());
+                }
+            }
+            User userVer = (User) userRepository.findByAccount_accountId(account.getAccountId());
+            if(userVer == null){
+                //Validate if a user hasn't been created by this account
+                return ResponseEntity.ok("error: User not found !");
+            } else if(userVer.getAddress().getAddressId() == null){
+                //Validate if this user has created it's address
+                return ResponseEntity.ok("error: This user does not own an Address");
+            } else if(userVer.getAddress().getAddressId() == addressService.findAddressById(id).map(Address::getAddressId).orElse(null)){
+                //Validate if this user is updating it's own address
+                return addressService.findAddressById(id)
+                        .map(ResponseEntity::ok)
+                        .orElse(ResponseEntity.notFound().build());
+            } else {
+                return ResponseEntity.ok("error: This user does not own this Address");
+            }
         }else{
             return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
         }
-        return addressService.findAddressById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<Address> createAddress(@RequestBody Address address, HttpServletRequest httpServletRequest){
-
-        Address savedAddress = addressService.createAddress(address);
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedAddress);
+    public ResponseEntity<?> createAddress(@RequestBody Address address, HttpServletRequest httpServletRequest){
+        if(httpServletRequest.getSession(false) != null){
+            //Validate if a session is in progress
+            HttpSession httpSession = httpServletRequest.getSession(false);
+            Account account = (Account) httpSession.getAttribute("newAccount");
+            User userVer = (User) userRepository.findByAccount_accountId(account.getAccountId());
+            //Validate if a user hasn't been created by this account
+            if(userVer == null){
+                return ResponseEntity.ok("error: User not found !");
+            //Validate if this user hasn't already created it's own address
+            } else if (userVer.getAddress() == null ){
+                Address savedAddress = addressService.createAddress(address);
+                //Assigning the created address to the user in session
+                userVer.setAddress(savedAddress);
+                userService.updateUser(userVer.getIdUser(),userVer);
+                return ResponseEntity.status(HttpStatus.CREATED).body(savedAddress);
+            } else {
+                return ResponseEntity.ok("error: This user already has an address");
+            }
+        }else{
+            return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
+        }
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Address> updateAddress(@PathVariable Long id, @RequestBody Address addressDetails, HttpServletRequest httpServletRequest){
-        return addressService.updateAddress(id, addressDetails)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    public ResponseEntity<?> updateAddress(@PathVariable Long id, @RequestBody Address addressDetails, HttpServletRequest httpServletRequest){
+        if(httpServletRequest.getSession(false) != null){
+            //Validate if a session is in progress
+            HttpSession httpSession = httpServletRequest.getSession(false);
+            Account account = (Account) httpSession.getAttribute("newAccount");
+            if(account.getRole().getRoleId() == 2) {
+                //Is a manager, can update any address
+                return addressService.updateAddress(id, addressDetails)
+                        .map(ResponseEntity::ok)
+                        .orElse(ResponseEntity.notFound().build());
+            }
+            User userVer = (User) userRepository.findByAccount_accountId(account.getAccountId());
+            if(userVer == null){
+                //Validate if a user hasn't been created by this account
+                return ResponseEntity.ok("error: User not found !");
+            } else if(userVer.getAddress() == null){
+                //Validate if this user has created it's address
+                return ResponseEntity.ok("error: This user does not own an Address");
+            } else if(userVer.getAddress().getAddressId() == addressService.findAddressById(id).map(Address::getAddressId).orElse(null)){
+                //Validate if this user is updating it's own address
+                return addressService.updateAddress(id, addressDetails)
+                        .map(ResponseEntity::ok)
+                        .orElse(ResponseEntity.notFound().build());
+            } else {
+                return ResponseEntity.ok("error: This user does not own this Address");
+            }
+        }else{
+            return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
+        }
     }
 }
