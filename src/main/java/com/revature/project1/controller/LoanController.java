4e7a2795d16bdd48180033aefb8e94a9a2c232f9
vale_tapiux <v.tapia@lasallistas.org.mx>
2025-03-25 09:00:16 -0600

    loan and user controller validation correction

--- src/main/java/com/revature/project1/controller/LoanController.java
+++ src/main/java/com/revature/project1/controller/LoanController.java
@@ -2,23 +2,25 @@ package com.revature.project1.controller;
 
 import com.revature.project1.Entities.Account;
 import com.revature.project1.Entities.Loan;
-import com.revature.project1.service.AccountService;
+import com.revature.project1.Entities.User;
+import com.revature.project1.repository.UserRepository;
 import com.revature.project1.service.LoanService;
+import com.revature.project1.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/loan")
 public class LoanController {
     private final LoanService loanService;
+    private final UserRepository userRepository;
 
-    public LoanController(LoanService loanService) {
+    public LoanController(LoanService loanService, UserRepository userRepository) {
         this.loanService = loanService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping
@@ -26,7 +28,7 @@ public class LoanController {
         if (httpServletRequest.getSession(false) != null){
             HttpSession httpSession = httpServletRequest.getSession(false);
             Account account = (Account) httpSession.getAttribute("newAccount");
-            if (account.getRole().getRoleId() == 2){
+            if (account.getRole().getRoleId() == 2){ // Only manager can see all Loans
                 return ResponseEntity.ok(loanService.findAllLoan());
             } else {
                 return ResponseEntity.ok("error: You have no permission to take this action!");
@@ -41,15 +43,21 @@ public class LoanController {
             HttpSession httpSession = httpServletRequest.getSession(false);
             Account account = (Account) httpSession.getAttribute("newAccount");
             if(account.getRole().getRoleId() == 2){
-                return loanService.findLoanById(id).map(ResponseEntity::ok)
-                        .orElse(ResponseEntity.notFound().build());
+                User userFromDB = userRepository.findByAccount_accountId(account.getAccountId());
+                if(loanService.findLoanById(id).isEmpty()){
+                    return ResponseEntity.ok("error: This loan does not exists");
+                }
+                if(userFromDB.getAccount().getAccountId() == loanService.findLoanById(id).map(Loan::getId).orElse(null)){
+                    return loanService.findLoanById(id).map(ResponseEntity::ok)
+                            .orElse(ResponseEntity.notFound().build());
+                } else{
+                    return ResponseEntity.ok("error: This user does not own this loan");
+                }
             }else {
                 return ResponseEntity.ok("error: You have no permission to take this action!");
             }
-        }
-        else {
+        } else {
             return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
-
         }
     }
 
@@ -58,15 +66,19 @@ public class LoanController {
         if(httpServletRequest.getSession(false) != null){
             HttpSession httpSession = httpServletRequest.getSession(false);
             Account account = (Account) httpSession.getAttribute("newAccount");
-            if(account.getRole().getRoleId() == 1){
-                Loan loanCreated = loanService.createLoan(loan);
-                return ResponseEntity.status(HttpStatus.CREATED).body(loanCreated);
+            if(account.getRole().getRoleId() == 1){ //Only customer can create loans
+                if(userRepository.findByAccount_accountId(account.getAccountId()) != null){
+                    Loan loanCreated = loanService.createLoan(loan);
+                    return ResponseEntity.status(HttpStatus.CREATED).body(loanCreated);
+                } else {
+                    return ResponseEntity.ok("error: User does not exists");
+                }
             } else {
                 return ResponseEntity.ok("error: You have no permission to take this action!");
             }
+        } else{
+            return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
         }
-        return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
-
     }
 
     @PutMapping("/{id}")
@@ -74,15 +86,15 @@ public class LoanController {
         if(httpServletRequest.getSession(false)!= null){
             HttpSession httpSession = httpServletRequest.getSession(false);
             Account account = (Account) httpSession.getAttribute("newAccount");
-            if(account.getRole().getRoleId() == 2){
+            if(account.getRole().getRoleId() == 2){ //only manager can update
                 return loanService.updateLoan(id, loanDetails)
                         .map(ResponseEntity::ok)
                         .orElse(ResponseEntity.notFound().build());
             } else {
                 return ResponseEntity.ok("error: As a manager you can only update a loan!");
-
             }
+        } else{
+            return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
         }
-        return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
     }
 }
--- src/main/java/com/revature/project1/controller/UserController.java
+++ src/main/java/com/revature/project1/controller/UserController.java
@@ -63,17 +63,18 @@ public class UserController {
         }
     }
 
-
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody User user, HttpServletRequest httpServletRequest){
         if (httpServletRequest.getSession(false) != null){
             HttpSession httpSession = httpServletRequest.getSession(false);
             Account account = (Account) httpSession.getAttribute("newAccount");
-            if (account.getRole().getRoleId() == 2) {
+            User userFromDB = (User) userService.findByAccountId(account.getAccountId());
+            //Validate if this account already created a user
+            if(userFromDB == null){
                 User userResponse = userService.createUser(user);
                 return ResponseEntity.status(HttpStatus.CREATED).body(userResponse);
-            }else {
-                return ResponseEntity.ok("error: You have no permission to take this action!");
+            } else{
+                return ResponseEntity.ok("error: This account already owns an user profile");
             }
         }
         else{
@@ -86,16 +87,25 @@ public class UserController {
         if (request.getSession(false) != null) {
             HttpSession session = request.getSession(false);
             Account account = (Account) session.getAttribute("newAccount");
-            if (account.getRole().getRoleId() == 2) {
+            if(account.getRole().getRoleId() == 2){
                 return ResponseEntity.ok(userService.updateUserPartial(id, updates));
-            } else {
-                return ResponseEntity.ok("error: You have no permission to take this action!");
+            } else{
+                User userFromDB = (User) userService.findByAccountId(account.getAccountId());
+                if(userFromDB != null){
+                    if(account.getAccountId() == userFromDB.getAccount().getAccountId()){
+                        User updatedUser = userService.updateUserPartial(id, updates);
+                        return ResponseEntity.ok(updatedUser);
+                    } else{
+                        return ResponseEntity.ok("error: This account does not own this user profile");
+                    }
+                } else{
+                    return ResponseEntity.ok("error: This account has not created the user profile");
+                }
             }
         } else {
             return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
         }
     }
-
 
     @PutMapping("/{id}")
     public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody User user, HttpServletRequest httpServletRequest) {
@@ -103,14 +113,23 @@ public class UserController {
             HttpSession httpSession = httpServletRequest.getSession(false);
             Account account = (Account) httpSession.getAttribute("newAccount");
             if (account.getRole().getRoleId() == 2) {
+                //Manager can update any user
                 User updatedUser = userService.updateUser(id, user);
                 return ResponseEntity.ok(updatedUser);
+            } else{
+                User userFromDB = (User) userService.findByAccountId(account.getAccountId());
+                if(userFromDB != null){
+                    if(account.getAccountId() == userFromDB.getAccount().getAccountId()){
+                        User updatedUser = userService.updateUser(id, user);
+                        return ResponseEntity.ok(updatedUser);
+                    } else{
+                        return ResponseEntity.ok("error: This account does not own this user profile");
+                    }
+                } else{
+                    return ResponseEntity.ok("error: This account has not created the user profile");
+                }
             }
-            else {
-                return ResponseEntity.ok("error: You have no permission to take this action!");
-            }
-        }
-        else{
+        } else{
             return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
         }
     }
@@ -134,14 +153,13 @@ public class UserController {
             userFromDB.setCreatedAt(user.getCreatedAt());
             User updatedUser = userService.updateUser(userFromDB.getIdUser(),userFromDB);
             return ResponseEntity.ok(updatedUser);
-
         }
         else{
             return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
         }
     }
 
-    @GetMapping("myInfo")
+    @GetMapping("/myInfo")
     public ResponseEntity<?> getMyUserInfo(HttpServletRequest httpServletRequest){
         if (httpServletRequest.getSession(false) != null){
             HttpSession httpSession = httpServletRequest.getSession(false);
@@ -152,12 +170,9 @@ public class UserController {
             }
             Optional<User> updatedUser = userService.getMyUserInfo(userFromDB.getIdUser());
             return ResponseEntity.ok(updatedUser);
-
         }
         else{
             return ResponseEntity.ok("error: Invalid action (no session is in progress)!");
         }
     }
-
-
 }
